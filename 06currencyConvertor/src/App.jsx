fd96332b5e9af4c66783f89c71b04ba7ee88901b
Milan Lamsal <milan123lamsal@gmail.com>
2025-03-14 19:21:30 -0400

    Components spelling corrected App.jsx

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -3,7 +3,7 @@ import { InputBox } from './Components'
 import useCurrencyInfo from './hooks/useCurrencyInfo'
 import { ThemeProvider } from './contexts/ThemeContext'
 import ThemeToggle from './Components/ThemeToggle'
-import ExchangeRateChart from './components/ExchangeRateChart'
+import ExchangeRateChart from './Components/ExchangeRateChart'
 
 function App() {
   //  count is a variable and setCount is a function , we gonna make custome hook
